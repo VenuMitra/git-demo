a3d854869bf2a27952726fb2707bda04687fa38b
Venu <venu2290.gv@gmail.com>
2021-07-07 16:13:46 +0530

    Added to local repositary

---
+++ src/test/java/com/git/test/git_demo/AppTest.java
@@ -0,0 +1,24 @@
+package com.git.test.git_demo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+	
+	public void test() {
+		System.out.println("Test program");
+	}
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
